eb8857e3fa0aab3442d25b8891484613e66402aa
Devansh Panthi <69043897+2709-devansh@users.noreply.github.com>
2020-09-02 12:19:02 +0530

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,20 @@
+class mango{
+    constructor(x,y,radius){
+        var options={
+            isStatic: true,
+            restitution: 0,
+            friction: 1
+        }
+        this.image = loadImage("Images/mango.png");
+        this.body = Bodies.circle(x,y,30,options);
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,this.radius,this.radius);
+        pop();
+    }
+}
